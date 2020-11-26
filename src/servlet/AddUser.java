f5bec95c5a53fc603a9bfc79710c5928c272ada9
j701u07 <j701u07@J701AT07.J504.kansai-center.ac.jp>
2020-11-26 15:45:15 +0900

    ◆IndexServlet新規作成 ◆AddUserServlet新規作成 ◆LoginServletの名前をControllerServletへ変更

---
+++ src/servlet/AddUser.java
@@ -0,0 +1,81 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class AddUser
+ */
+@WebServlet("/AddUser")
+public class AddUser extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public AddUser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//■GET通信
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		//■POST通信（ユーザ登録画面から）***********************************
+		//◇データ（userId,pass）があれば登録する
+
+		//パラメータから情報を取得
+		request.setCharacterEncoding("UTF-8");
+		String userName = request.getParameter("userName");
+		String passWord = request.getParameter("passWord");
+
+		//データの有無の確認
+		if (userName != null && userName.length() != 0) {//氏名の入力有
+			if (passWord != null && passWord.length() != 0) {//パスワードの入力有
+				//DBにユーザ情報を登録
+				//UserDAO dao = new UserDAO();
+				//dao.addUser(userName, passWord);
+
+				//セッションスコープに氏名とパスワードを保存
+				HttpSession session = request.getSession();
+				session.setAttribute("userName", userName);//氏名
+
+				//一般ユーザログイン画面にフォワード
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/YourShelf/LoginOK.jsp");
+				dispatcher.forward(request, response);
+
+			} else {//パスワード入力なし
+				//TOP画面にフォワード
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/YourShelf/Index.java");
+				dispatcher.forward(request, response);
+			}
+		} else {//氏名入力なし
+			//TOP画面にフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/YourShelf/Index.java");
+			dispatcher.forward(request, response);
+
+		}
+
+	}
+}
--- src/servlet/ControllerServlet.java
+++ src/servlet/ControllerServlet.java
@@ -17,13 +17,13 @@ import model.LoginLogic;
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/LoginServlet")
-public class LoginServlet extends HttpServlet {
+public class ControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public LoginServlet() {
+	public ControllerServlet() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
