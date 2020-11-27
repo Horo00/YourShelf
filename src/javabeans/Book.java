ce0b0425401fb80e9985bfb04c1bc879be8bac8c
j701u05 <j701u05@J701AT05.J504.kansai-center.ac.jp>
2020-11-27 11:09:27 +0900

    BookのJavabeans作成

---
+++ src/javabeans/Book.java
@@ -0,0 +1,64 @@
+package javabeans;
+
+public class Book {
+	private String title;
+	private String writer;
+	private int count;
+	private String company;
+	private String imgUrl;
+	private boolean isCheckedOut;
+
+	public Book() {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public boolean isCheckedOut() {
+		return isCheckedOut;
+	}
+
+	public void setCheckedOut(boolean isCheckedOut) {
+		this.isCheckedOut = isCheckedOut;
+	}
+
+
+}
